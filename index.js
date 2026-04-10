05e41a1f3ceb9ef54f2e196296d2f42c56d66bb1
Dazed31 <darrenamore13@gmail.com>
2026-04-10 12:48:03 +0300

    Add combineUsers utility to merge multiple username arrays with merge date

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+require("datejs");
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  // loop through each array and merge users
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [
+      ...combinedObject.users,
+      ...args[i]
+    ];
+  }
+
+  // add merge date using datejs format M/d/yyyy
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
